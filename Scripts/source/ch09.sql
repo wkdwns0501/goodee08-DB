620fa3dae1204b74a9cdb9a00a6cd0b9fe3fba58
wkdwns0501 <britain0501@naver.com>
2025-12-18 17:27:30 +0900

    서브쿼리

--- Scripts/source/ch09.sql
+++ Scripts/source/ch09.sql
@@ -96,3 +96,369 @@ WHERE 컬럼명 연산자 (
 -- ③ 서브쿼리는 단일 값만 반환한다. (  )
 
 -- 정답: O O X
+
+/*
+	9.2 다양한 위치에서의 서브쿼리
+*/
+-- 1. SELECT 절에서의 서브쿼리
+-- 1x1 단일값만 반환하는 서브쿼리(스칼라 서브쿼리)만 사용 가능
+-- 이유: 여러 행 또는 여러 컬럼을 반환하면 어떤 값을 선택해야 할 지 몰라서 에러 발생
+
+-- => SELECT 절의 서브쿼리는 결과 집합의 각 행에 대해
+-- 	  하나의 컬럼 값을 생성해야 하므로 반드시 1행 1컬럼(스칼라 값)만 반환 가능
+-- 여러 행 또는 여러 컬럼을 반환할 경우
+--   어떤 값을 해당 컬럼에 넣어야 할지 결정할 수 없어 에러 발생
+
+
+-- 모든 결제 정보에 대한 평균 결제 금액과의 차이는?
+SELECT
+	payment_type AS '결제 유형',
+	amount AS '결제 금액',
+	amount - (평균결제금액) AS '평균 결제 금액과의 차이'
+FROM payments;
+
+-- 평균 결제 금액
+SELECT AVG(amount) FROM payments;
+
+-- () 괄호 안에 서브쿼리 넣기
+SELECT
+	payment_type AS '결제 유형',
+	amount AS '결제 금액',
+	amount - (SELECT AVG(amount) FROM payments) AS '평균 결제 금액과의 차이'
+FROM payments;
+
+-- 잘못된 사용 예
+-- SELECT에 사용하는 서브쿼리는 스칼라 서브쿼리만 가능
+SELECT
+	payment_type AS '결제 유형',
+	amount AS '결제 금액',
+	-- amount - (SELECT AVG(amount), '123' FROM payments) AS '평균 결제 금액과의 차이' 
+	-- 다중 컬럼 서브쿼리
+	amount - (SELECT amount FROM payments) AS '평균 결제 금액과의 차이' 
+	-- 다중 행 서브쿼리
+FROM payments;
+-- SQL Error [1241] [21000]: Operand should contain 1 column(s) -- 다중 컬럼 서브쿼리
+-- SQL Error [1242] [21000]: Subquery returns more than 1 row -- 다중 행 서브쿼리
+
+-- 2. FROM 절에서의 서브쿼리
+-- NxM 반환하는 행과 컬럼의 개수에 제한이 없음
+-- 실행 결과가 마치 하나의 독립된 가상 테이블(View)처럼 사용되기 때문에 테이블 서브쿼리라고 부름
+-- 단, 서브쿼리에 별칭 지정 필수
+
+-- 1회 주문 시 평균 상품 개수는? (장바구니 상품 포함)
+-- 일단 먼저 1회 주문 당 상품 개수 집계 구하기
+-- 1단계: 주문별(order_id)로 그룹화 -> count 집계: SUM() 
+-- 2단계: 재집계: AVG()
+SELECT 
+	order_id,
+	SUM(count) AS total_count
+FROM order_details
+GROUP BY order_id; -- 서브쿼리로 사용(테이블 서브쿼리라고 부름)
+
+-- 메인쿼리: 1회 주문 시 평균 상품 개수 집계
+SELECT
+	AVG(sub.total_count) -- 별칭으로 접근(권장)
+-- 	AVG(sub.`SUM(count)`) -- 접근 가능(비권장)
+FROM (
+	-- 서브쿼리
+	SELECT 
+		order_id,
+		SUM(count) AS total_count 
+		-- 외부 쿼리에서 집계 결과를 참조하기 위해 별칭 사용(가독성 + 호환성)
+-- 		SUM(count) 
+		-- SUM(count)라는 컬럼명 자동 생성
+	FROM order_details
+	GROUP BY order_id
+) AS sub; -- 별칭 필수(AS는 생략 가능)
+
+-- 3. JOIN 절에서의 서브쿼리
+-- NxM 반환하는 행과 컬럼의 개수에 제한이 없음
+-- 실행 결과가 마치 하나의 독립된 가상 테이블(View)처럼 사용되기 때문에 테이블 서브쿼리라고 부름
+-- 단, 서브쿼리에 별칭 지정 필수
+
+-- 상품별 주문 개수를 '배송 완료'와 '장바구니'에 상관없이 상품명과 주문 개수를 조회한다면?
+-- 일단 먼저 상품 아이디별 주문 개수 집계 구하기
+SELECT
+	product_id,
+	SUM(count) AS total_count
+FROM order_details
+GROUP BY product_id;
+
+-- 메인쿼리: 상품명을 포함한 상품별 주문 개수 집계
+SELECT 
+	p.name AS 상품명,
+	sub.total_count AS '주문 개수' -- 서브쿼리에서 구한 데이터를 가져다 씀
+FROM products p
+JOIN (
+	-- 서브쿼리
+	SELECT
+		product_id,
+		SUM(count) AS total_count
+	FROM order_details
+	GROUP BY product_id
+) AS sub ON p.id = sub.product_id;
+
+-- 또 다른 방법: 일단 필요한 테이블을 붙여놓고(JOIN) 그룹화 및 집계
+SELECT 
+	p.name AS '상품명',
+	SUM(count) AS '주문 개수'
+FROM products p 
+JOIN order_details od ON p.id = od.product_id
+GROUP BY p.id, p.name; -- 이식성을 고려한 권장 코드: 명시적으로 GROUP BY 에 포함
+
+-- 4. WHERE 절에서의 서브쿼리
+-- 1x1, Nx1 반환하는 서브쿼리만 사용 가능(필터링 조건으로 값 또는 값의 목록을 사용하기 때문)
+
+-- 평균 가격보다 비싼 상품을 조회하려면?
+SELECT *
+FROM PRODUCTS
+WHERE price > (평균가격);
+-- 평균 가격을 서브쿼리로 구해서 넣으면 됨
+SELECT *
+FROM PRODUCTS
+WHERE price > (
+	-- 서브 쿼리
+	SELECT AVG(price) FROM products
+);
+
+-- 5. HAVING 절에서의 서브쿼리
+-- 1x1, Nx1 반환하는 서브쿼리만 사용 가능(필터링 조건으로 값 또는 값의 목록을 사용하기 때문)
+
+-- 크림 치즈보다 메출이 높은 상품은? (장바구니 상품 포함)
+-- 상품x주문 상세 조인해서 -> 상품명으로 그룹화 -> 상품별로 매출을 집계
+-- 메인쿼리: 전체 상품의 매출을 조회
+SELECT 
+	p.name AS 상품명,
+    SUM(price * count) AS 매출
+FROM products p
+JOIN order_details od ON p.id = od.product_id
+GROUP BY p.name;
+
+-- => 크림 치즈보다 매출이 높은 상품 조회로 변경
+SELECT 
+	p.name AS 상품명,
+    SUM(price * count) AS 매출
+FROM products p
+JOIN order_details od ON p.id = od.product_id
+GROUP BY p.name
+HAVING SUM(price * count) > (
+	-- 서브쿼리: 크림 치즈의 매출(=8720)
+    SELECT SUM(price * count)
+	FROM products p
+	JOIN order_details od ON p.id = od.product_id
+	WHERE p.name = '크림 치즈'
+);
+
+-- Quiz
+-- 2. 다음 설명이 맞으면 O, 틀리면 X를 표시하세요.
+-- ① SELECT 절의 서브쿼리는 단일 값만 반환해야 한다. (  )
+-- ② FROM 절과 JOIN 절의 서브쿼리는 별칭을 지정해야 한다. (  )
+-- ③ WHERE 절과 HAVING 절의 서브쿼리는 단일 값 또는 다중 행의 단일 컬럼을 반환할 수 있다. (  )
+
+-- 정답: O O O
+
+/*
+	9.3 IN, ANY, ALL, EXISTS
+*/
+-- 목록을 다룰 수 있는 특별한 연산자
+-- 주로 WHERE 절에서의 서브쿼리와 쓰임
+
+-- 1. IN 연산자
+-- 괄호 사이 목록에 포함되는 대상을 찾음
+
+-- 형식
+컬럼명 IN (쉼표로 구분된 값 목록);
+-- 또는
+컬럼명 IN (다중 행의 단일 컬럼을 반환하는 서브쿼리);
+
+-- IN 연산자 사용 예1: 값 목록을 입력받는 경우
+-- Quiz: 상품명이 '우유 식빵', '크림 치즈'인 대상의 id 목록은?
+SELECT id
+FROM products
+WHERE name IN ('우유 식빵', '크림 치즈');
+
+-- IN 연산자 사용 예2: 서브쿼리를 입력받는 경우
+-- '우유 식빵', '크림 치즈'를 포함하는 주문의 상세 내역
+SELECT *
+FROM order_details
+WHERE product_id IN (
+	-- 서브쿼리: 우유 식빵과 크림 치즈의 아이디를 반환(Nx1)
+	SELECT id
+	FROM products
+	WHERE name IN ('우유 식빵', '크림 치즈')
+);
+
+-- IN 연산자 사용 예3: 조인과 IN 연산자
+-- '우유 식빵', '크림 치즈'를 주문한 사용자 아이디와 닉네임은?
+SELECT DISTINCT u.id, u.nickname
+FROM users u
+JOIN orders o ON u.id = o.user_id
+JOIN order_details od ON o.id = od.order_id
+JOIN products p ON od.product_id = p.id
+WHERE p.name IN ('우유 식빵', '크림 치즈')
+ORDER BY u.id;
+
+-- 2. ANY 연산자
+-- 지정된 집합의 모든 요소와 비교 연산(>, < 등)을 수행하여 하나라도 만족하는 대상을 찾음
+
+-- 형식
+컬럼명 비교연산자 ANY (다중 행의 단일 컬럼을 반환하는 서브쿼리);
+
+-- '우유 식빵'이나 '플레인 베이글'보다 저렴한 상품 목록은?
+-- 메인쿼리: 
+SELECT name AS 이름, price AS 가격
+FROM products
+WHERE price < ANY (
+	-- 서브쿼리: 우유 식빵이나 플레인 베이글의 가격 조회
+	SELECT price 
+	FROM products 
+	WHERE name IN ('우유 식빵', '플레인 베이글') -- 2900, 1300
+); -- 결과적으로 최대값보다 작으면 참 (2900원 보다 작은 모든 상품 조회)
+
+-- 집계 함수 사용으로 대체
+SELECT name AS 이름, price AS 가격
+FROM products
+WHERE price < (
+	-- 서브쿼리: 우유 식빵이나 플레인 베이글의 가격 조회
+	SELECT MAX(price) 
+	FROM products 
+	WHERE name IN ('우유 식빵', '플레인 베이글') -- 2900
+);
+
+-- 3. ALL 연산자
+-- 지정된 집합의 모든 요소와 비교 연산(>, < 등)을 수행하여 모두 만족하는 대상을 찾음
+
+-- 형식
+컬럼명 비교연산자 ALL (다중 행의 단일 컬럼을 반환하는 서브쿼리);
+
+-- '우유 식빵'이나 '플레인 베이글'보다 비싼 상품 목록은?
+-- 메인쿼리: 
+SELECT name AS 이름, price AS 가격
+FROM products
+WHERE price > ALL (
+	-- 서브쿼리: 우유 식빵과 플레인 베이글의 가격 조회
+	SELECT price 
+	FROM products 
+	WHERE name IN ('우유 식빵', '플레인 베이글') -- 2900, 1300
+); -- 결과적으로 최대값보다 크면 참 (2900원 보다 큰 모든 상품 조회)
+
+-- 집계 함수 사용으로 대체
+SELECT name AS 이름, price AS 가격
+FROM products
+WHERE price > (
+	-- 서브쿼리: 우유 식빵과 플레인 베이글의 가격 조회
+	SELECT MAX(price) 
+	FROM products 
+	WHERE name IN ('우유 식빵', '플레인 베이글') -- 2900
+);
+
+-- 정리: 두 쿼리 모두 ANY, ALL을 사용했을 때와 완전히 동일한 결과를 반환
+-- ANY, ALL 보다는 집계 함수를 사용한 코드가 더 직관적이고 이해하기 쉬움
+-- 그래서 ANY, ALL 사용 빈도는 IN 이나 집계 함수에 비해 낮은 편
+
+-- 4.EXISTS 연산자
+-- 서브쿼리 결과 행이 1개 이상이면 TRUE => 단 하나의 행이라도 반환하면 서브쿼리 중단하고 TRUE
+-- 서브쿼리 결과 행이 0개이면 FALSE => 아무 행도 반환하지 않으면 FALSE
+-- 서브쿼리가 반환하는 결과값 자체에는 관심이 없고, 
+-- 		오직 서브쿼리의 결과로 행이 하나라도 존재하는지 여부만 체크
+
+-- 형식
+SELECT 컬럼명1, 컬럼명2, ...
+FROM  테이블명
+WHERE EXISTS (서브쿼리);
+
+-- EXISTS 연산자 실습
+-- 적어도 1번 이상 주문한 사용자를 조회하려면?
+-- 메인쿼리
+SELECT 
+FROM users u
+WHERE EXISTS (
+	-- 서브쿼리: 주문자 아이디가 사용자 테이블에 있다면 1반환
+	SELECT 1 
+	-- 굳이 모든 컬럼을 다 가져오는 것보다 1만 써주는게 효율적이고 관례적
+	-- (EXISTS 연산에서 값 자체는 의미가 없기 때문)
+	FROM orders o 
+	WHERE o.user_id = u.id 
+	-- 이렇게 서브쿼리가 메인쿼리의 특정 값을 참조하는 쿼리를 '상관 서브쿼리'라고 함
+	-- 여기서 '상관'의 의미는 메인쿼리와 서브쿼리가 서로 영향을 준다는 뜻
+);
+
+-- 위 상관 서브쿼리 해석
+-- users 테이블 1번 사용자의 주문이 있는지
+-- orders 테이블을 반복하면 확인, 있으면 결과 테이블에 1 반환
+-- EXISTS는 결과가 하나라도 존재하면 TRUE가 되기 때문에, 매칭되는 레코드를 찾으면 더 이상 검사하지 않음
+-- 같은 방식으로 2번 사용자, 3번 사용자도 확인
+
+-- (정리) 서브쿼리의 동작 방식
+-- 비상관 서브쿼리: 서브쿼리가 단 한 번 실행된 후, 그 결과를 메인쿼리가 사용
+-- 상관 서브쿼리:
+-- 1) 메인쿼리가 먼저 한 행을 읽고
+-- 2) 읽혀진 행의 값을 서브쿼리에 전달하여, 서브쿼리가 실행됨
+-- 3) 서브쿼리 결과를 이용해 메인쿼리의 WHERE 조건을 판단
+-- 4) 메인쿼리의 다음 행을 읽고, 2-3번 과정을 반복
+
+-- (참고) 상관 서브쿼리의 특징
+-- 1) 의존성: 서브쿼리는 메인쿼리의 값을 참조해 데이터 필터링을 수행
+-- 2) 반복 실행: 서브쿼리는 메인쿼리의 각 행에 대해 반복적으로 실행됨
+-- 3) 성능 저하: 메인쿼리의 각 행마다 서브쿼리를 실행하므로 쿼리 전체의 성능이 저하될 수 있음
+-- 특히 메인쿼리 테이블의 데이터 양이 많을 경우 사용 지양
+-- 메인쿼리의 행 수만큼 서브쿼리가 반복 실행되기 때문
+
+-- 또 다른 방법1
+SELECT DISTINCT user_id 
+FROM orders;
+
+SELECT * 
+FROM users 
+WHERE id IN (
+	SELECT DISTINCT user_id 
+	FROM orders -- 서브쿼리의 결과값이 많아질수록 성능 저하
+);
+
+-- 또 다른 방법2(권장)
+SELECT DISTINCT u.*
+FROM users u
+JOIN orders o ON u.id = o.user_id;
+-- 상관 서브쿼리보다 JOIN이 더 효율적
+
+-- (정리) 실무에서 서브쿼리 vs JOIN 
+-- 일반적으로 데이터베이스는 JOIN이 서브쿼리보다 성능이 더 좋거나 최소한 동일
+-- 1) JOIN을 우선적으로 고려
+-- 2) JOIN으로 표현하기 너무 복잡하거나, 서브쿼리의 가독성이 훨씬 좋다면 서브쿼리를 사용
+
+-- NOT EXISTS 연산자 실습
+-- 서브쿼리 결과가 0행이면 TRUE, 한 건이라도 있으면 FALSE
+-- 결과행 1개만 발견되면 바로 FALSE (서브쿼리 중단)
+
+-- 'COCOA PAY'로 결제하지 않은 사용자를 조회하려면?
+-- 메인쿼리
+SELECT *
+FROM users u 
+WHERE NOT EXISTS (
+	-- 서브쿼리: COCOA PAY를 사용한 사용자가 있다면 1을 반환
+ 	SELECT 1
+ 	FROM orders o 
+ 	JOIN payments p ON o.id = p.order_id
+ 	WHERE o.user_id = u.id AND payment_type = 'COCOA PAY'
+);
+-- 의미: 주문과 결제 테이블에서 COCOA PAY를 사용한 사용자가 있다면
+-- 	 	 그 사용자를 제외한 사용자 조회
+
+-- Quiz
+-- 3. 다음 빈칸에 들어갈 용어를 고르세요.
+-- ① __________: 지정된 집합에 포함되는 대상을 찾음
+-- ② __________: 별칭을 지정하는 키워드로, 생략하고 사용할 수 있음
+-- ③ __________: 지정된 집합의 모든 요소와 비교 연산을 수행해 하나라도 만족하는 대상을 찾음
+-- ④ __________: 지정된 집합의 모든 요소와 비교 연산을 수행해 모두를 만족하는 대상을 찾음
+-- ⑤ __________: 서브쿼리를 입력받아 서브쿼리가 하나 이상의 행을 반환할 경우 TRUE, 그렇지 않으면 FALSE 반환
+
+-- (ㄱ) AS
+-- (ㄴ) ANY
+-- (ㄷ) IN
+-- (ㄹ) ALL
+-- (ㅁ) EXISTS
+
+-- 정답: ㄷ ㄱ ㄴ ㄹ ㅁ
+
+
+
